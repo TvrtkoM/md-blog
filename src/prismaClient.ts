028b87ec161b6c7e1a9c6c2ccf7f0db6707316dc
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-03-21 19:56:46 +0100

    fix multiple instances of prismaClient

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+let prismaClient: PrismaClient;
+
+declare global {
+  var prismaClient: PrismaClient | undefined;
+}
+
+//check if we are running in production mode
+if (process.env.NODE_ENV === "production") {
+  prismaClient = new PrismaClient();
+} else {
+  //check if there is already a connection to the database
+  if (!global.prismaClient) {
+    global.prismaClient = new PrismaClient();
+  }
+  prismaClient = global.prismaClient;
+}
 
 export default prismaClient;
