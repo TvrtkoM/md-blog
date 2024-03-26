8a9af8ed85c590842e9b2464604e6fb64455340a
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-03-26 20:01:59 +0100

    alert styling

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -6,15 +6,17 @@ import { useDispatch } from "react-redux";
 
 const Alert = () => {
   const message = useAlertMessageSelector();
+  // const message =
+  //   "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium";
   const dispatch = useDispatch();
   return (
     message && (
-      <div className="fixed top-0 container bg-destructive text-stone-50 text-sm h-8">
-        <div className="h-full flex items-center justify-between">
-          <p>{message}</p>
+      <div className="fixed top-12 right-12 w-56 rounded-sm p-3 bg-destructive text-stone-50 text-sm shadow-lg">
+        <div className="flex items-center justify-between">
+          <p className="w-4/5">{message}</p>
           <IoIosClose
             size={32}
-            className="cursor-pointer"
+            className="cursor-pointer mb-auto"
             onClick={() => {
               dispatch(hideMessage());
             }}
--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 const AuthContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
     <section className="container max-w-screen-sm mt-10 px-4">
-      <div className="bg-stone-300 rounded-lg text-stone-700 shadow-lg px-6 py-7 border border-indigo-200">
+      <div className="bg-stone-300 rounded-lg text-stone-700 shadow-md px-6 py-7 border border-indigo-200">
         {children}
       </div>
     </section>
