f9d8292b4883871adf8ac06a47a4a99139fd7428
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-03-26 19:33:11 +0100

    change padding

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 const AuthContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <section className="container max-w-screen-sm mt-10">
+    <section className="container max-w-screen-sm mt-10 px-4">
       <div className="bg-stone-300 rounded-lg text-stone-700 shadow-lg px-6 py-7 border border-indigo-200">
         {children}
       </div>
