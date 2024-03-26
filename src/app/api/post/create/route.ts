7ae4e089311c97ec13c8b8301b24359d3d94a37a
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-03-26 21:21:34 +0100

    basic create post api

--- src/app/api/post/create/route.ts
+++ src/app/api/post/create/route.ts
@@ -1,22 +1,30 @@
-import { invalidToken } from "@/lib/server-errors";
-import { verifyAccessToken } from "@/lib/tokens";
+import { validationError } from "@/lib/server-errors";
+import { getAccessTokenDataOrError } from "@/lib/tokens";
+import { PostSchema } from "@/zod-schemas/post";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import prismaClient from "@/prismaClient";
 
 export async function POST(req: NextRequest) {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("token")?.value;
-  if (!accessToken) {
-    return NextResponse.json(null, { status: 401 });
-  }
-  try {
-    const payload = verifyAccessToken(accessToken);
-    if (typeof payload !== "string" && payload.userId) {
-      const { userId } = payload;
+  const userData = getAccessTokenDataOrError(cookies());
+  if (typeof userData === "number") {
+    const userId = userData;
+    const data = await req.json();
+
+    const postData = await PostSchema.safeParseAsync(data);
+
+    if (!postData.success) {
+      return validationError(postData.error.issues);
     } else {
-      return invalidToken();
+      const post = await prismaClient.post.create({
+        data: {
+          content: postData.data.content,
+          userId
+        }
+      });
+      return NextResponse.json(post, { status: 201 });
     }
-  } catch {
-    return invalidToken();
+  } else {
+    return userData;
   }
 }
