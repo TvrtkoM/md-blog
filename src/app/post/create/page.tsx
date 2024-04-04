6c0ec93cec5fc2701fd2f737ef5f4839d8d855aa
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-04-04 20:43:44 +0200

    fix content frame

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ContentFrame from "@/components/ContentFrame";
 import CreatePostForm from "@/components/forms/CreatePostForm";
 import MainContainer from "@/components/MainContainer";
 import withAuthGuard from "@/withAuthGuard";
@@ -6,10 +7,12 @@ import withAuthGuard from "@/withAuthGuard";
 const CreatePostPage = () => {
   return (
     <MainContainer>
-      <h2 className="text-xl font-semibold pb-3 border-b border-stone-400 mb-6">
-        Create new post
-      </h2>
-      <CreatePostForm />
+      <ContentFrame>
+        <h2 className="text-xl font-semibold pb-3 border-b border-stone-400 mb-6">
+          Create new post
+        </h2>
+        <CreatePostForm />
+      </ContentFrame>
     </MainContainer>
   );
 };
