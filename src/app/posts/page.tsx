8cdd824b8abc1a91980897f9d6b6235ec1565c5f
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-04-10 21:42:42 +0200

    no need for suspense here

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,19 +3,16 @@
 import MainContainer from "@/components/containers/MainContainer";
 import Post from "@/components/Post";
 import { usePostsQuery } from "@/queries/usePostsQuery";
-import { Suspense } from "react";
 
 const PostsPage = () => {
   const { data: posts } = usePostsQuery();
   return (
     <MainContainer>
-      <Suspense>
-        <div className="flex flex-col space-y-4">
-          {posts.map((post) => {
-            return <Post post={post} key={post.id} />;
-          })}
-        </div>
-      </Suspense>
+      <div className="flex flex-col space-y-4">
+        {posts.map((post) => {
+          return <Post post={post} key={post.id} />;
+        })}
+      </div>
     </MainContainer>
   );
 };
