dcb2e607e9c9e7926a75910c3e72e983c66d79c9
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2024-04-15 18:52:41 +0200

    summary max 250 characters

--- src/zod-schemas/post.ts
+++ src/zod-schemas/post.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const PostSchema = z.object({
   id: z.number().optional(),
   content: z.string().min(1, "Required"),
-  summary: z.string().optional(),
+  summary: z.string().max(250, "Maximum 250 characters"),
   title: z
     .string()
     .min(12, "Minimum 12 characters")
